49f73aef9fec64f29e41eb0037d98412c5aa21b7
kirtivpatil <kirtivpatil@gmail.com>
2022-05-26 11:11:08 -0400

    2nd for master

--- src/test/java/grp1/Seleniumpractice/newtest.java
+++ src/test/java/grp1/Seleniumpractice/newtest.java
@@ -2,8 +2,10 @@ package grp1.Seleniumpractice;
 
 public class newtest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		
+		Thread.sleep(1000);
 
 	}
 
