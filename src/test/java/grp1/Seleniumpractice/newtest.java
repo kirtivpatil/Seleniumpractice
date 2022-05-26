c5fae75e89627ad9078ec0db755e7ea998b1c68a
kirtivpatil <kirtivpatil@gmail.com>
2022-05-26 11:19:59 -0400

    for branch

--- src/test/java/grp1/Seleniumpractice/newtest.java
+++ src/test/java/grp1/Seleniumpractice/newtest.java
@@ -2,9 +2,10 @@ package grp1.Seleniumpractice;
 
 public class newtest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-
+Thread.sleep(2000);
+System.out.println("Merge Conflcit");
 	}
 
 }
